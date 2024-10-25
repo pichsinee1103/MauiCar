cc9b0019a767657d600c328ee32ca0fc81bd971a
USER <USER@LAPTOP-40HHQMF6>
2024-10-25 17:21:07 +0700

    Add project files.

---
+++ CarView/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace CarView
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
